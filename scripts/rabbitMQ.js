bfb181318ac8d41d35f71785d2f2c9c2c1eea72f
jimting <handsome841206@gmail.com>
2019-04-25 16:33:30 +0800

    Update rabbitMQ.js

--- scripts/rabbitMQ.js
+++ scripts/rabbitMQ.js
@@ -6,7 +6,7 @@ module.exports = function(robot) {
 	sub.on('data', function(note) {
 		var json = JSON.parse(note);
 		var user_data = { "room": json.roomNumber, "user_id": json.userID};
-		String result = "[公告]專案 " + json.build_name + "第"+json.build_number+"次建置剛剛執行了！建置結果："+json.build_status;
+		var result = "[公告]專案 " + json.build_name + "第"+json.build_number+"次建置剛剛執行了！建置結果："+json.build_status;
 		robot.send(user_data,result)
 	});
 }
