d986f3fb74b30b5dba2fe2ee7de8705959852170
Jimting <handsome841206@gmail.com>
2019-07-25 23:27:41 +0800

    Update init_bot.js

--- scripts/init_bot.js
+++ scripts/init_bot.js
@@ -2,7 +2,14 @@
 * @param {object} data
 */
 
-module.exports = function(robot) {
+module.exports = function(robot) 
+{
+    init_bot(robot);
+}
+
+function init_bot(robot)
+{
+    
     var hubotAnalyze = require('./hubotAnalyze').hubotAnalyze;
     //get all bot data from mongodb
     var botData = [];
@@ -16,9 +23,7 @@ module.exports = function(robot) {
             botData = result;
             //build and init bots
             var SlackBot = require('slackbots');
-            var bots = robot.brain.get('bots');
-            if(bots == null)
-                bots = [];
+            var bots = [];
             
             console.log(botData)
             console.log("總共有"+botData.length+"個機器人資料");
@@ -35,35 +40,86 @@ module.exports = function(robot) {
                 });
                 var tempBot = {bot:bot, token:token, name:name, data:botData[i]};
                 bots.push(tempBot);
-                robot.brain.set("bots", bots);
-                console.log(bots);
-				
-                bots[i]["bot"].on('message', function(data) 
+                
+                bot.on('message', function(data) 
                 {
-					var team_name = team;
-                    if(data.type!="error")
-						console.log(data);
+                    var team_name = team;
+                    if(data.type=="error")
+                    {
+                        //先找出這個訊息是哪個group收到的，找出列表中的位置，目前只能一個一個檢查QAQ
+                        var bots = robot.brain.get('bots');
+                        for(var k = 0;k < bots.length;k++)
+                        {
+                            if(data.team == bots[k].data.team_id)
+                            {
+                                var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+                                robot.send(admin_data,"("+bots[k].data.team_name+")有錯誤訊息 : "+JSON.stringify(data));
+                                //bot.wsUrl = ev.url;
+                            }
+                        }
+                    }
                     if(data.type=="message")
                     {
                         if(data.subtype!='bot_message' && data.bot_id==null)
                         {
                             //先找出這個訊息是哪個group收到的，找出列表中的位置，目前只能一個一個檢查QAQ
-							var bots = robot.brain.get('bots');
-							for(var k = 0;k < bots.length;k++)
-							{
-								if(data.team == bots[k].data.team_id)
-								{
-									var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
-									robot.send(admin_data,"("+bots[k].data.team_name+")有新活動:");
-									hubotAnalyze(bots[k].bot, robot, data, bots[k].data.team_name);
-								}
-							}
+                            var bots = robot.brain.get('bots');
+                            for(var k = 0;k < bots.length;k++)
+                            {
+                                if(data.team == bots[k].data.team_id)
+                                {
+                                    var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+                                    robot.send(admin_data,"("+bots[k].data.team_name+")有新活動/使用者編號 "+data.user+" 在 聊天室編號 "+data.channel+" 說 "+data.text);
+                                    hubotAnalyze(bots[k].bot, robot, data, bots[k].data.team_name);
+                                }
+                            }
                         }
                     }
-
+                    if(data.type == 'reconnect_url') {
+                        var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+                        robot.send(admin_data,"(收到重新連線的資料) : "+JSON.stringify(data));
+                        //先找出這個訊息是哪個group收到的，找出列表中的位置，目前只能一個一個檢查QAQ
+                        var bots = robot.brain.get('bots');
+                        for(var k = 0;k < bots.length;k++)
+                        {
+                            if(data.team == bots[k].data.team_id)
+                            {
+                                var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+                                robot.send(admin_data,"("+bots[k].data.team_name+")重新連接的URL : "+data.url);
+                                //bot.wsUrl = ev.url;
+                            }
+                        }
+                    }
+                });
+                bot.on('close', function() {
+                    reset_bot(robot);
+                });
+                bot.on('error', function(data) 
+                {
+                    var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+                    robot.send(admin_data,"(Error occur) :" + data);
                 });
             }
+            
+            robot.brain.set("bots", bots);
+            console.log(bots);
             db.close();
         });
     }); 
+}
+
+function reset_bot(robot)
+{
+    var bots = robot.brain.get('bots');
+    if(bots == null)
+        bots = [];
+    var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+    robot.send(admin_data,"遇到連線問題，開始重新設定機器人");
+    
+    for(var i = 0; i < bots.length;i++)
+    {
+        var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+        robot.send(admin_data,"("+bots[i].data.team_name+")重新連線");
+        bots[i].bot.connect();
+    }
 }
