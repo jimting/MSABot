a2995787dd28ed5373736628078099ef52c491db
Jimting <handsome841206@gmail.com>
2019-09-24 16:16:27 +0800

    Update hello.js

--- scripts/hello.js
+++ scripts/hello.js
@@ -3,4 +3,27 @@ module.exports = function(robot) {
     re = ['Hello', 'hello', 'Hi', 'hi', '你好', '你好啊', '安安', '哈囉','你期待我回覆你什麼？','你以為我是打招呼機器人嗎？','好','打招呼？好',response.match[1]];
     response.send("@"+response.envelope.user.name+":"+response.random(re));
   });
+  
+  robot.hear(/(analyze)\s(.*)/, function(response) {
+    var analyze_string = response.match[2];
+	var request = require('request');
+
+	var options = {
+	  uri: 'http://140.121.197.134:5005/webhooks/rest/webhook',
+	  method: 'POST',
+	  json: {
+		"message": analyze_string
+	  }
+	};
+	
+	request(options, function (error, res, body) 
+	{
+	  if (!error && res.statusCode == 200) 
+		response.send("@"+response.envelope.user.name+":"+body);
+	  if(error)
+		response.send("[error] @"+response.envelope.user.name+":"+error);
+	});
+
+   
+  });
 }
