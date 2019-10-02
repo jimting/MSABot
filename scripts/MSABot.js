3db4d283335ee32e4757e753d6755c6401e5356e
Jimting <handsome841206@gmail.com>
2019-10-03 00:17:15 +0800

    type 1.0

--- scripts/MSABot.js
+++ scripts/MSABot.js
@@ -49,7 +49,7 @@ var init_bot = function(robot)
 
 var new_bot = function(token, name, robot, team_name)
 {
-    var hubotAnalyze = require('./Positive_messageAnalyzer').hubotAnalyze;
+    var hubotAnalyze = require('./Positive_messageFlow').hubotAnalyze;
     var SlackBot = require('slackbots');
     var bot = new SlackBot({
         token: token, // Add a bot https://my.slack.com/services/new/bot and put the token 
@@ -60,7 +60,7 @@ var new_bot = function(token, name, robot, team_name)
         // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
         var welcome = ['我又回來了。請先停看聽。', '振作起來好嗎。我剛回來。', '我的老天鵝。我回來了。', '我回來了。', '我回來了。派對收攤。', '我又來了。期待您的使用 ( ͡° ͜ʖ ͡°)', '安安', '嗖。 我剛剛著陸下來。','我剛回來。 可以幫我補血嗎？','挑戰者來了，我 來 也！','我來了。 請給我一罐啤酒。','嘿！快聽！我回來了！', '我剛回來。 似乎太 OP - 請 nerf 一下。', '一個人走很危險，跟我一起走吧！', '我上來了！我又下去了！我又上來了！怎麼樣！我可以一直上來下來！很厲害吧！', '槓上開花加一台（主機）'];
         var welcomeRandom = Math.floor(Math.random()*(welcome.length-1));
-        bot.postMessageToChannel('general', "[機器人重新連線]"+welcome[welcomeRandom]);
+        //bot.postMessageToChannel('general', "[機器人重新連線]"+welcome[welcomeRandom]);
     });
     bot.on('message', function(data) 
     {
---
+++ scripts/Positive_messageFlow.js
@@ -0,0 +1,191 @@
+exports.hubotAnalyze =  function(bot, robot, data, team_name)
+{
+   console.log(data);
+   mentionAndAnalyze(bot, robot, data, team_name);
+}
+
+/* If mention then do something*/
+function mentionAndAnalyze(bot, robot, data, team_name)
+{
+	var bot_id = "@"+bot.self.id;
+	var result = data.text.match(bot_id);
+    if(result != null)
+    {
+		var stage = robot.brain.get('stage'+data.channel);
+		switch(stage)
+		{
+			// if got stage 1, means that the channel lack the service name. and the next input should include the service name XD
+			case 2 :
+				var intent = robot.brain.get('intent'+data.channel);
+				stage1(bot, robot, data, team_name, intent);
+				break;
+			default : 
+				stage0(bot, robot, data, team_name);
+		}
+    }
+}
+
+// stage 0 : if analyze no intent, ask the intent, if have, go stage1
+// stage 1 : analyze the intent result, if have service name, skip stage2 to mission
+// stage 2 : if lack service name, ask service name.
+// stage 3 : mission.
+
+/* send request and check if result has intent */
+function stage0(bot, robot, data, team_name)
+{
+	// setting the stage first.
+	robot.brain.set("stage"+data.channel, 0);
+	
+	var request = require('request');
+	var options = {
+	  uri: 'http://140.121.197.134:4112/webhooks/rest/webhook',
+	  method: 'POST',
+	  json: 
+	  {
+		"message": data.text
+	  }
+	};
+	
+	request(options, function (error, res, body) 
+	{
+		if (!error && res.statusCode == 200) 
+		{
+			// parse the ' to ", because Rasa always return json with '
+			var json_data = JSON.parse((body[0].text).replace(/'/g, '"'));
+			console.log(json_data);
+			var intent = json_data.intent;
+			var service = json_data.service;
+			// check what result it is ! 
+			// json : {'intent': 'action_name', 'service': 'service_name'}
+			// check if the result include the service name.
+			if(intent != "none")
+			{
+				stage1(bot, robot, data, team_name, intent);
+			}
+			else
+			{
+				var result = "Sorry, I don't know what you wanna do accurately. Please retry again!";
+				bot.postMessage(data.channel, result);
+				// end the flow
+			}
+		}
+		if(error)
+		{
+			var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+			robot.send(admin_data,"Rasa Server is inactive! Please check it!");
+			bot.postMessage(data.channel, "Sorry, the server got something wrong. I'll be back in minutes! QAQ");
+			// end the flow
+		}
+	});
+}
+
+/*check if the result has service name*/
+function stage1(bot, robot, data, team_name, intent)
+{
+	// setting the stage and intent first.
+	robot.brain.set("stage"+data.channel, 1);
+	robot.brain.set("intent"+data.channel, 0);
+	
+	if(service=="none" | service==null)
+	{
+		stage2(bot, robot, data, team_name, intent);
+	}
+	else
+	{
+		switch(intent)
+		{
+			case "action_service_health"	:action_service_health(bot, robot, data, team_name, intent, service);break;
+			case "action_service_info"		:action_service_info(bot, robot, data, team_name, intent, service);break;
+			case "action_service_using_info":action_service_using_info(bot, robot, data, team_name, intent, service);break;
+			case "action_service_api_list"	:action_service_api_list(bot, robot, data, team_name, intent, service);break;
+			case "action_service_env"		:action_service_env(bot, robot, data, team_name, intent, service);break;
+		}
+	}
+}
+
+function stage2(bot, robot, data, team_name, intent)
+{
+	// setting the stage first.
+	robot.brain.set("stage", 2);
+	var intentStr = "";
+	switch(intent)
+	{
+		case "action_service_health"	:intentStr = "service's health data";break;
+		case "action_service_info"		:intentStr = "service's information";break;
+		case "action_service_using_info":intentStr = "service's using overview";break;
+		case "action_service_api_list"	:intentStr = "service's api list";break;
+		case "action_service_env"		:intentStr = "service's env setting";break;
+	}
+	var result = "Hey, before we catch the "+intentStr+", we need to know what service you wanna search. ";
+	bot.postMessage(data.channel, result);
+	var 
+	//end the flow
+}
+
+function action_service_health(bot, robot, data, team_name, service)
+{
+	var fs = require("fs");
+	fs.readFile('service_health.txt', 'utf8', function(err, data) 
+	{
+		datas = data.split("\r\n");
+		for(var data in datas)
+			console.log(datas[data])
+    });
+	var json_data = JSON.parse(datas);
+	var result = "Hey, the " + service + " 's health data is down below: \n";
+	result += "The status to this service : " + json_data.status + "\n";
+	result += "The Composite Discovery Client is : " + json_data.discoveryComposite.status;
+	result += "The Eureka Server is : " + json_data.discoveryComposite.eureka.status;
+	result += "The hytrix status is : " + json_data.hystrix.status;
+	var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
+	robot.send(admin_data,"Rasa Server is inactive! Please check it!");
+	bot.postMessage(data.channel, result);
+}
+
+function action_service_info(bot, robot, data, team_name, service)
+{
+	var fs = require("fs");
+	fs.readFile('service_health.txt', 'utf8', function(err, data) 
+	{
+		datas = data.split("\r\n");
+		for(var data in datas)
+			console.log(datas[data])
+    });
+	var json_data = JSON.parse(datas);
+}
+
+function action_service_using_info(bot, robot, data, team_name, service)
+{
+	var fs = require("fs");
+	fs.readFile('service_health.txt', 'utf8', function(err, data) 
+	{
+		datas = data.split("\r\n");
+		for(var data in datas)
+			console.log(datas[data])
+    });
+	var json_data = JSON.parse(datas);
+}
+
+function action_service_api_list(bot, robot, data, team_name, service)
+{
+	var fs = require("fs");
+	fs.readFile('service_health.txt', 'utf8', function(err, data) 
+	{
+		datas = data.split("\r\n");
+		for(var data in datas)
+			console.log(datas[data])
+    });
+	var json_data = JSON.parse(datas);
+}
+
+function action_service_env(bot, robot, data, team_name, service)
+{
+	var fs = require("fs");
+	fs.readFile('service_health.txt', 'utf8', function(err, data) 
+	{
+		datas = data.split("\r\n");
+		for(var data in datas)
+			console.log(datas[data])
+    });
+	var json_data = JSON.parse(datas);
+}
