38a6aaa2e144bba6cd046fbcbca03d2d3e2c2716
Jimting <handsome841206@gmail.com>
2019-10-02 21:57:12 +0800

    Update Positive_messageAnalyzer.js

--- scripts/Positive_messageAnalyzer.js
+++ scripts/Positive_messageAnalyzer.js
@@ -17,6 +17,15 @@ function mentionAndAnalyze(bot, robot, data, team_name)
 
 function getAnalyzeResult(bot, robot, data, team_name)
 {
+	// setting all the result to false
+	var status = { "action_service_health":false, "action_service_info":false, "action_service_using_info":false, "action_service_api_list":false, "action_service_env":false };
+	
+	// setting the stage
+	// stage 0 : if analyze no intent, ask the intent.
+	// stage 1 : analyze the intent
+	// stage 2 : if lack service name, ask service name.
+	var stage = 0;
+	
 	var request = require('request');
 	var options = {
 	  uri: 'http://140.121.197.134:4112/webhooks/rest/webhook',
@@ -30,7 +39,10 @@ function getAnalyzeResult(bot, robot, data, team_name)
 	{
 		if (!error && res.statusCode == 200) 
 		{
-			bot.postMessage(data.channel, body[0].text).then(function(d) 
+			// check what result it is ! 
+			// json : {'intent': 'action_name', 'service': 'service_name'}
+			//switch(body[0].text['service']){}
+			bot.postMessage(data.channel, body[0].text['intent']).then(function(d) 
 			{
 				var admin_data = { "room": "D9PCFGPH9", "user_id": "handsome841206"};
 				robot.send(admin_data,"("+team_name+")Analyze result : "+ body[0].text);
