40a4d59dff7d7b46fbf69b56823b270801f90bc5
Jimting <handsome841206@gmail.com>
2019-10-16 14:59:30 +0800

    Update Positive_messageFlow.js

--- scripts/Positive_messageFlow.js
+++ scripts/Positive_messageFlow.js
@@ -80,6 +80,7 @@ function mentionAndAnalyze(bot, robot, data, team_name)
 {
 	var bot_id = "@"+bot.self.id;
 	var result = data.text.match(bot_id);
+	data.text = data.text.replace(bot_id, "");
     if(result != null)
     {
 		stage0(bot, robot, data, team_name);
