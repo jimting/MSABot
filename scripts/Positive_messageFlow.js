5e6ff2a58f9052cf88f294fa73aec5b7cce354bb
Jimting <handsome841206@gmail.com>
2019-11-27 14:41:18 +0800

    update the selenium env

--- scripts/Positive_messageFlow.js
+++ scripts/Positive_messageFlow.js
@@ -708,9 +708,15 @@ function action_dependency_graph(bot, robot, data, team_name)
 }
 
 async function getVMAMVGraphBase64(bot, data, url, system_name) {
+	let path = require('geckodriver').path;
+
+	let service = new firefox.ServiceBuilder(path).build();
+	firefox.setDefaultService(service);
+
 	let driver = await new Builder()
         .forBrowser('firefox')
 		.setFirefoxOptions(new firefox.Options().headless().windowSize(screen))
+		.withCapabilities(webdriver.Capabilities.firefox())
         .build();
 
 	await driver.get(url);
